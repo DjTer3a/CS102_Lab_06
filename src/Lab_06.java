08d0f86410ffbbf8c89a06e50e4283d4f164068b
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-07-26 09:32:51 +0400

    Question 3 completed

--- src/Lab_06.java
+++ src/Lab_06.java
@@ -1,5 +1,7 @@
+import java.util.*;
 
 public class Lab_06 {
+
     public static int stringLength(String string){
         if(string.equals("")){
             return 0;
@@ -29,15 +31,58 @@ public class Lab_06 {
         }
     }
 
+    public static void createBinary(int start,  int size, char[] ch){
+        if(start == size){
+            System.out.println(toString(ch)+" ");
+            return;
+        }
+
+        if(ch[size-1] == '0'){
+            ch[size] = '0';
+            createBinary(start, size+1, ch);
+            ch[size] = '1';
+            createBinary(start, size+1, ch);
+        }
+
+        if(ch[size-1] == '1'){
+            ch[size] = '0';
+
+            createBinary(start, size+1, ch);
+        }
+    }
+
+    public static void allBinary(int start){
+        if(start <=0){
+            return;
+        }
+
+        char[] ch = new char[start];
+
+        ch[0] = '0';
+        createBinary(start, 1, ch);
+
+        ch[0] = '1';
+        createBinary(start, 1, ch);
+
+    }
+
+    public static String toString(char[] ch){
+        String string = new String(ch);
+        return string;
+    }
+
     public static void main(String[] args){
 
         String string = "CS102 is the best";
         System.out.println(stringLength(string));
         
-        String string2 = "CS102 is a good course";
+        String string2 = "CS102 is start good course";
         System.out.println(stringLength(string2));
         System.out.println(countNonVowels(string2));
         System.out.println(string2.length()-countNonVowels(string2));
+
+        int num = 4;
+        allBinary(num);
 
     }
 
